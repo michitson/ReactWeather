3ddc12becc73241d5734aff9a00f02bee44cab99
Andrew Michitson <michitson@yahoo.com>
2016-12-19 23:01:22 +0000

    added copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -2,7 +2,10 @@ var React = require('react');
 
 var Examples = (props) =>{
     return(
-      <h2> Examples component</h2>
+      <div>
+        <h3> Examples component</h3>
+        <p>Welcome to Examples Page</p>
+      </div>
     );
   }
 
